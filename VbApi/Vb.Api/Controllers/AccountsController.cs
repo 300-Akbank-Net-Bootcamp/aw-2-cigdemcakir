8f130ba9698c230d6b61d7c1163aaca5a738fdd3
cigdemcakir <cigdemcakir4897@gmail.com>
2023-12-29 17:11:36 +0300

    Homework 2 Added.

---
+++ VbApi/Vb.Api/Controllers/AccountsController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vb.Data;
+using Vb.Data.Entity;
+
+namespace VbApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : ControllerBase
+{
+    private readonly VbDbContext dbContext;
+
+    public AccountsController(VbDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<List<Account>> Get()
+    {
+        return await dbContext.Set<Account>()
+            .ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<Account> Get(int id)
+    {
+        var account =  await dbContext.Set<Account>()
+            .Include(a => a.AccountTransactions)
+            .Include(a => a.EftTransactions)
+            .Where(x => x.Id == id).FirstOrDefaultAsync();
+       
+        return account;
+    }
+
+    [HttpPost]
+    public async Task Post([FromBody] Account account)
+    {
+        await dbContext.Set<Account>().AddAsync(account);
+        await dbContext.SaveChangesAsync();
+    }
+
+    [HttpPut("{id}")]
+    public async Task Put(int id, [FromBody] Account account)
+    {
+        var fromdb = await dbContext.Set<Account>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        
+        dbContext.Entry(account).State = EntityState.Modified;
+        
+        await dbContext.SaveChangesAsync();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete(int id)
+    {
+        var fromdb = await dbContext.Set<Account>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        
+        fromdb.IsActive = false;
+        
+        await dbContext.SaveChangesAsync();
+    }
+}
